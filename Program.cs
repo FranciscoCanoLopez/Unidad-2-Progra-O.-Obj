bce2c2a5bff5cb1568b247b37a6b711677b916b7
Francisco Cano <volublecoffe@gmail.com>
2024-02-19 18:02:47 -0600

    second commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using physics_problem
+;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculate the speed of a car");
+        Console.WriteLine("Enter the distance traveled (in meters)");
+        double distance = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter time (in seconds)");
+        double time = Convert.ToDouble(Console.ReadLine());
+
+        physical physical = new physical();
+        double speed = physical.Calculate_Speed(distance, time);
+
+        Console.WriteLine("The speed of the car is: " + speed + " meters per second.");
+    }
+}
---
+++ physical.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physics_problem
+{
+    public class physical
+    {
+        public double Calculate_Speed(double distance, double time)
+        {
+            return distance / time;
+        }
+    }
+}
